23c7f9a4a13a5ab76b64b352dfc481d35ade2a72
James Lee <lee963654@gmail.com>
2023-09-14 15:41:27 -0400

    started migrations create user

--- backend/db/migrations/20230908001614-create-user.js
+++ backend/db/migrations/20230908001614-create-user.js
@@ -16,10 +16,14 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       username: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        unique: true
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(256),
+        allowNull: false,
+        unique: true
       },
       hashedPassword: {
         type: Sequelize.STRING
