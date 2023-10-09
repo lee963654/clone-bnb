bcc87f32997b079dde14552f0d4b0bd01833f622
James Lee <lee963654@gmail.com>
2023-10-09 18:46:38 -0400

    started frontend method 1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,13 @@
+// frontend/src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
+
 import './index.css';
+
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
 import configureStore from './store';
 
 const store = configureStore();
@@ -14,11 +18,11 @@ if (process.env.NODE_ENV !== "production") {
 
 function Root() {
   return (
-    <ReduxProvider store={store}>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ReduxProvider>
+    </Provider>
   );
 }
 
