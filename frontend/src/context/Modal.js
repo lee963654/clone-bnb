c71508b55ec7f51b6735e321e04133685ec1f74c
James Lee <lee963654@gmail.com>
2023-11-15 20:05:12 -0500

    started on context folder modal

---
+++ frontend/src/context/Modal.js
@@ -0,0 +1,15 @@
+// frontend/src/context/Modal.js
+import React, { useRef } from "react";
+
+const ModalContext = React.createContext();
+
+export function ModalProvider({ children }) {
+  const modalRef = useRef();
+
+  return (
+    <>
+      <ModalContext.Provider>{children}</ModalContext.Provider>
+      <div ref={modalRef} />
+    </>
+  );
+}
