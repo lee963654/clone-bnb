c1c2ba39f78eafb6ea2b5848f71079d7fcd0a5b3
James Lee <lee963654@gmail.com>
2023-11-17 19:42:12 -0500

    updated modal

--- frontend/src/context/Modal.js
+++ frontend/src/context/Modal.js
@@ -5,9 +5,14 @@ const ModalContext = React.createContext();
 
 export function ModalProvider({ children }) {
   const modalRef = useRef();
+  const [modalContent, setModalContent] = useState(null);
+  const [onModalClose, setOnModalClose] = useState(null);
 
   const contextValue = {
     modalRef, // reference to modal div
+    modalContent, // React component to render inside modal
+    setModalContent, // function to set the React component to render inside modal
+    setOnModalClose, // function to set the callback function to be called when modal is closing
   };
 
   return (
