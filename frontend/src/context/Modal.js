0e578af6007a40d669513c7bbdf45cb2099f7b48
James Lee <lee963654@gmail.com>
2023-12-30 15:39:12 -0500

    updated modal.js to export usemodal

--- frontend/src/context/Modal.js
+++ frontend/src/context/Modal.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useContext } from "react";
 import ReactDOM from "react-dom";
-import "./Modal.css"
+import "./Modal.css";
 
 const ModalContext = React.createContext();
 
@@ -53,3 +53,5 @@ export function Modal() {
     modalRef.current
   );
 }
+
+export const useModal = () => useContext(ModalContext);
